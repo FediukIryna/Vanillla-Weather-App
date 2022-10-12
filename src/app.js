175aad252e9189e54b67b596dcb4085138f7c498
FediukIryna <irynafedyuk12@gmail.com>
2022-10-12 23:57:41 +0300

    Updated time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours <10){
+        hours=`0${hours}`; 
+    }
+    let minutes = date.getMinutes();
+    if(minutes <10){
+        minutes=`0${minutes}`; 
+    }
+    let days = ["Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thuesday",
+      "Friday",
+      "Saturday"];
+
+    let day = days[date.getDay()];
+    return `${day}  ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -9,8 +32,9 @@ function displayTemperature(response){
     let humidityElement= document.querySelector("#humidity");
     humidityElement.innerHTML=response.data.main.humidity;
     let windElement= document.querySelector("#wind");
-    windElement.innerHTML=response.data.wind.speed;
-
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+    let dateElement= document.querySelector("#date");
+    dateElement.innerHTML=formatDate(response.data.dt*1000);
 }   
 
 let apiKey="e4e8b1ed839a508aac0f7edc43c3cd9f";
