51b7e2eadfa418c4e23c1edbaef9835cb506b295
FediukIryna <irynafedyuk12@gmail.com>
2022-10-16 22:24:37 +0300

    added function getForecast for daily forecast

--- src/app.js
+++ src/app.js
@@ -20,6 +20,12 @@ function formatDate(timestamp){
     return `${day}  ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+    let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
@@ -37,10 +43,12 @@ function displayTemperature(response){
     dateElement.innerHTML=formatDate(response.data.dt*1000);
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    getForecast(response.data.coord);
 }   
 
 function search(city){
-    let apiKey="e4e8b1ed839a508aac0f7edc43c3cd9f";
+    let apiKey="1a6432c5ca7b6f9b0bee45c98d54ea71";
     let apiUrl =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
@@ -72,7 +80,8 @@ function displayCalsiusTempetarure(event){
     fahrenheitLink.classList.remove("active");
 }
 
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data);
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`;
     let days = ["Mon", 
@@ -116,4 +125,3 @@ let calsiusLink = document.querySelector("#calsius-link");
 calsiusLink.addEventListener("click", displayCalsiusTempetarure);
 
 search("New York");
-displayForecast();
